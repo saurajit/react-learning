6ed0d53694cda962451dfb203c9b899e13d0aa9a
saura <saurajitb@gmail.com>
2020-04-21 19:25:49 +0530

    Added secondary app for testing

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const Hello = () => (
-  <h1>Hello World</h1>
+  <div>
+    <h1>Hello World</h1>
+    <p>
+      This is from app 1
+    </p>
+  </div>
 )
 
 ReactDOM.render(
-  <div>
-    <Hello />
-  </div>,
+  <Hello />,
   document.getElementById('root')
 )
---
+++ secondary-app.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  React.createElement('h1', null, 'Hello World!! in the second app'),
+  document.getElementById('secondary-app')
+)
