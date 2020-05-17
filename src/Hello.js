9efea9674473cc8b257806a951eb5be1dfa02ff9
Saurajit <saurajitb@gmail.com>
2020-05-18 00:14:12 +0530

    Added the app using create-react-app

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hello() {
+    console.log('In here')
+    return (
+        <div>
+            Hello from component
+        </div>
+    )
+}
+
+export default Hello;
