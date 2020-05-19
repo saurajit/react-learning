0df45ffc252838c6e9bb9b4eff82d06ca3b1d637
Saurajit <saurajitb@gmail.com>
2020-05-19 15:01:37 +0530

    Added SCSS for the styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app-wrapper">
       </div>
     );
   }
